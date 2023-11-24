d979293bf8bc7d5d4641448163177b3187c84276
Hamed <hamedpyn@gmail.com>
2023-11-24 17:04:10 +0330

    fix: images bugs

--- src/Components/Chart/XAxisUser.jsx
+++ src/Components/Chart/XAxisUser.jsx
@@ -14,31 +14,31 @@ const XAxis = [
 ]
 
 const TransActions = [
-    { avatar:"/public/profile.jfif",btnTitle: "Approved", id: 1, title: "Hamed Pooryousef", date: "Jun 12, 2023", amount: 129, type: "success", },
-    { avatar:"/public/qadir.jpg",btnTitle: "Failure", id: 2, title: "Ali Daei", date: "Nov 29, 2023", amount: 156, type: "failure", },
-    { avatar:"/public/sasan.jpg",btnTitle: "Pending", id: 3, title: "Lionel Messi", date: "Feb 19, 2023", amount: 142, type: "blue", },
-    { avatar:"/public/hamze.jpg",btnTitle: "Approved", id: 4, title: "Javad Nekonam", date: "Mar 23, 2023", amount: 134, type: "success", },
+    { avatar:"./profile.jfif",btnTitle: "Approved", id: 1, title: "Hamed Pooryousef", date: "Jun 12, 2023", amount: 129, type: "success", },
+    { avatar:"./qadir.jpg",btnTitle: "Failure", id: 2, title: "Ali Daei", date: "Nov 29, 2023", amount: 156, type: "failure", },
+    { avatar:"./sasan.jpg",btnTitle: "Pending", id: 3, title: "Lionel Messi", date: "Feb 19, 2023", amount: 142, type: "blue", },
+    { avatar:"./hamze.jpg",btnTitle: "Approved", id: 4, title: "Javad Nekonam", date: "Mar 23, 2023", amount: 134, type: "success", },
 ]
 
 const newUsers = [
-    { id: 1, title: "Hamed Pooryousef", job: "Web Developer",avatar:"/public/profile.jfif" },
-    { id: 2, title: "Ali Daei", job: "Football Player",avatar:"/public/qadir.jpg" },
-    { id: 3, title: "Lionel Messi", job: "Football Player",avatar:"/public/sasan.jpg" },
-    { id: 4, title: "Javad Nekonam", job: "Football Player",avatar:"/public/hamze.jpg" },
+    { id: 1, title: "Hamed Pooryousef", job: "Web Developer",avatar:"./profile.jfif" },
+    { id: 2, title: "Ali Daei", job: "Football Player",avatar:"./qadir.jpg" },
+    { id: 3, title: "Lionel Messi", job: "Football Player",avatar:"./sasan.jpg" },
+    { id: 4, title: "Javad Nekonam", job: "Football Player",avatar:"./hamze.jpg" },
 ]
 
 const UserList = [
-    { id: 1, title: "Hamed Pooryousef", status: "active", amount: 129,avatar:"/public/profile.jfif" },
-    { id: 2, title: "Ali Daei", status: "non-active", amount: 156,avatar:"/public/qadir.jpg" },
-    { id: 3, title: "Lionel Messi", status: "active", amount: 142,avatar:"/public/sasan.jpg" },
-    { id: 4, title: "Javad Nekonam", status: "non-active", amount: 134,avatar:"/public/hamze.jpg" },
+    { id: 1, title: "Hamed Pooryousef", status: "active", amount: 129,avatar:"./profile.jfif" },
+    { id: 2, title: "Ali Daei", status: "non-active", amount: 156,avatar:"./qadir.jpg" },
+    { id: 3, title: "Lionel Messi", status: "active", amount: 142,avatar:"./sasan.jpg" },
+    { id: 4, title: "Javad Nekonam", status: "non-active", amount: 134,avatar:"./hamze.jpg" },
 ]
 
 const allProducts = [
-    {id:1,title:"DELL",price:1199,img:"/public/dell.jpg"},
-    {id:2,title:"ASUS",price:1399,img:"/public/asus.jpeg"},
-    {id:3,title:"HP",price:1299,img:"/public/hp.jpg"},
-    {id:4,title:"ACER",price:1599,img:"/public/acer.jpg"},
+    {id:1,title:"DELL",price:1199,img:"./dell.jpg"},
+    {id:2,title:"ASUS",price:1399,img:"./asus.jpeg"},
+    {id:3,title:"HP",price:1299,img:"./hp.jpg"},
+    {id:4,title:"ACER",price:1599,img:"./acer.jpg"},
 ]
 
 const monthSales = [
@@ -48,10 +48,10 @@ const monthSales = [
 ]
 
 const productsDetails = [
-    {stock:"no",action:"yes",id:1,title:"DELL",price:1199,img:"/public/dell.jpg"},
-    {stock:"yes",action:"no",id:2,title:"ASUS",price:1399,img:"/public/asus.jpeg"},
-    {stock:"no",action:"yes",id:3,title:"HP",price:1299,img:"/public/hp.jpg"},
-    {stock:"yes",action:"no",id:4,title:"ACER",price:1599,img:"/public/acer.jpg"},
+    {stock:"no",action:"yes",id:1,title:"DELL",price:1199,img:"./dell.jpg"},
+    {stock:"yes",action:"no",id:2,title:"ASUS",price:1399,img:"./asus.jpeg"},
+    {stock:"no",action:"yes",id:3,title:"HP",price:1299,img:"./hp.jpg"},
+    {stock:"yes",action:"no",id:4,title:"ACER",price:1599,img:"./acer.jpg"},
 ]
 
 export { XAxis, TransActions, newUsers, UserList,allProducts,monthSales,productsDetails };
